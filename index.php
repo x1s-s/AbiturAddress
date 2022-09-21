19231bab4d4a32ba857b4cf4b984aefb386fc843
Aliaksei Shkurko <shaleprog@gmail.com>
2022-09-21 18:36:59 +0300

    edit input

--- index.php
+++ index.php
@@ -5,23 +5,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
     <title>Document</title>
 </head>
 
 <body>
+    <button onclick="editInput('Show')">Нажми меня</button>
     <div class="dropdown">
-        <button onclick="myFunction()" class="dropbtn">Dropdown</button>
         <div id="myDropdown" class="dropdown-content">
-            <input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()">
+            <input type="text" id="myInput" onblur="endOut()" onfocus="startOut()" onkeyup="filterFunction()">
             <?php
             $array = array("Gomel", "Minsk", "Grodno", "Vitebsk", "Mogilev", "Brest");
             foreach ($array as $value) {
-                echo "<a href='#'>" . $value . "</a>";
+                echo "<a style=\"display: none\" onclick=\"editInput('" . $value . "')\">" . $value . "</a>";
             }
             ?>
         </div>
     </div>
+    <script>
+        function editInput(value) {
+            document.getElementById("myInput").value = value;
+        }
+    </script>
+    <script>
+        function startOut() {
+            var a = document.getElementById("myDropdown").getElementsByTagName("a");
+            for (var i = 0; i < a.length; i++) {
+                a[i].style.display = "block";
+            }
+        }
+    </script>
+    <script>
+        function endOut() {
+            setTimeout(function() {
+                var a = document.getElementById("myDropdown").getElementsByTagName("a");
+                for (var i = 0; i < a.length; i++) {
+                    a[i].style.display = "none";
+                }
+            }, 250);
+        }
+    </script>
     <script>
         function filterFunction() {
             var input, filter, ul, li, a, i;
@@ -44,7 +67,6 @@
             document.getElementById("myDropdown").classList.toggle("show");
         }
     </script>
-
 </body>
 
 </html>
