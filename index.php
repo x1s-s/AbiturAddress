c8da4f2122ade677677c84d1faf415759728cece
Aliaksei Shkurko <shaleprog@gmail.com>
2022-09-21 16:30:44 +0300

    delete dropbox.js

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel="js" href="dropbox.js">
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
